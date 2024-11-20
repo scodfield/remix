41f8abde028bb5a78983c7ef324686243d0c97a5
scodfield <953836918@qq.com>
2024-11-20 20:52:59 +0800

    full test

---
+++ contracts/0_ZeroGas.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title ZeroGas
+ * @dev HelloWeb3 
+ */
+ contract ZeroGas {
+    function SayHi() public pure returns (string memory){
+        return "Hello Web3. From ethd";
+    }
+ }
---
+++ scripts/contract.js
@@ -0,0 +1,16 @@
+var contractAddress = '0xD4CaDB5A47306a08aCa77294d132c93711B94382';
+var contractAbi = [
+	{
+		"inputs": [],
+		"name": "SayHi",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	}
+]
